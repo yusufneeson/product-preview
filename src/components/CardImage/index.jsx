2f06f0abeb69939699bea86aa91bc19d276ff39b
yusufneeson <yusufneeson@gmail.com>
2022-08-25 16:06:23 +0700

    responsive but not complete

--- src/components/CardImage/index.jsx
+++ src/components/CardImage/index.jsx
@@ -1,14 +1,37 @@
 import Image from "next/future/image";
+import { useEffect, useState } from "react";
 import style from "./cardimage.module.css";
 
 function CardImage() {
+	const [width, setWidth] = useState(0);
+
+	const updateWidth = () => {
+		setWidth(window.innerWidth);
+	};
+
+	useEffect(() => {
+		updateWidth();
+		window.addEventListener("resize", updateWidth);
+		return () => window.removeEventListener("resize", updateWidth);
+	}, []);
+
 	return (
 		<div className={style.cardimage}>
-			<Image
-				src="/images/image-product-desktop.jpg"
-				width={302}
-				height={500}
-			/>
+			{width > 626 ? (
+				<Image
+					src="/images/image-product-desktop.jpg"
+					width={302}
+					height={500}
+					priority={true}
+				/>
+			) : (
+				<Image
+					src="/images/image-product-mobile.jpg"
+					width={345}
+					height={241}
+					priority={true}
+				/>
+			)}
 		</div>
 	);
 }
