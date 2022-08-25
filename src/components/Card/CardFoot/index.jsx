b418929c1438d689b293f2e74f2e90da822044d1
yusufneeson <yusufneeson@gmail.com>
2022-08-25 15:15:21 +0700

    mount button component in cardFoot

--- src/components/Card/CardFoot/index.jsx
+++ src/components/Card/CardFoot/index.jsx
@@ -1,11 +1,17 @@
+import Button from "../../Button";
 import style from "./cardfoot.module.css";
 
 function CardFoot() {
 	return (
-		<div className={style.cardfoot}>
-			<span className={style.price}>$149.99</span>
-			<s className={style.discounted}>$169.99</s>
-		</div>
+		<>
+			<div className={style.cardfoot}>
+				<span className={style.price}>$149.99</span>
+				<s className={style.discounted}>$169.99</s>
+			</div>
+			<div className={style.cta}>
+				<Button text="Add to Cart" />
+			</div>
+		</>
 	);
 }
 
