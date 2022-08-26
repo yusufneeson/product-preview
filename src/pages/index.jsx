3d4e29097b9626e1ed8b2e95cf78cab1e6d26f95
yusufneeson <yusufneeson@gmail.com>
2022-08-26 15:06:38 +0700

    change flex to grid

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import Box from "../components/Box";
 import Home from "../layouts/home";
+import Head from "next/head";
 
 export default function Index() {
 	return (
-		<Home>
-			<Box />
-		</Home>
+		<>
+			<Head>
+				<title>Product Preview</title>
+			</Head>
+			<Home>
+				<Box />
+			</Home>
+		</>
 	);
 }
