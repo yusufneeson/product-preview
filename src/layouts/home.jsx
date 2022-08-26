7a27ac88f34ac09cb87471cd2f71670ffae26de5
yusufneeson <yusufneeson@gmail.com>
2022-08-26 15:52:46 +0700

    added repository link

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,7 +1,13 @@
+import Author from "../components/author/author";
 import style from "./home.module.css";
 
 function Home({ children }) {
-	return <div className={style.home}>{children}</div>;
+	return (
+		<div className={style.home}>
+			{children}
+			<Author />
+		</div>
+	);
 }
 
 export default Home;
